22239a1865eae55ee70514ddb00d391193050ee6
Replit user <>
2022-08-09 17:56:23 +0000

    primer ejercicio de laboratorio

---
+++ sesion1/ejercicio1.py
@@ -0,0 +1,19 @@
+'''
+*********** Curso de programación acelerada en Python ************
+Date 04-08-2022
+File: sesion/ejercicio1.py
+Autor: Programador x
+Action: asignación de variables
+'''
+nombre_estado = 'Tabasco'
+superficie = 25267
+poblacion_total = 2403000000
+promedio_temperatura = 31.3
+estados_cercanos = ['Chiapas','Campeche',' Veracruz']
+productos_produce = ['Cacao',  'Coco', 'Caña']
+print(nombre_estado, "es un estado que ", )
+print("colinda al sur", "con", estados_cercanos[0], "y")
+print("Tiene una superficie de ", superficie) 
+print("Población total: ", poblacion_total)
+print("Promedio de temperatura: ", promedio_temperatura)
+print("Productos que produce: ", productos_produce[0], ',', productos_produce[1], 'y', productos_produce[2] )
